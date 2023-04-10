dc294ac0cf66783b81d2c7ac2fb75ebbc8d6f11c
HapzDev <adarsh@hapzsoft.com>
2023-04-10 15:34:34 +0530

    Complete Project

---
+++ Assets/JellyGarden/Scripts/GUI/GameFieldAnim.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameFieldAnim : MonoBehaviour {
+
+    private void Start()
+    {
+        
+    }
+
+    void EndAnimGamField()
+    {
+       LevelManager.THIS.gameStatus = GameState.Playing;
+    }
+}
